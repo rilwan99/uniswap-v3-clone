73f8d8c4ca3e2dee1996db2a87c4d076e717a393
rilwan99 <rilwanwild@gmail.com>
2023-02-20 18:53:11 +0800

    update test cases

---
+++ test/ERC20Mintable.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.14;
+
+import {ERC20} from "solmate/src/tokens/ERC20.sol";
+
+contract ERC20Mintable is ERC20 {
+
+    constructor(
+        string memory _name, 
+        string memory _symbol, 
+        uint8 _decimals
+    ) ERC20(_name, _symbol, _decimals) {}
+
+    function mint(address to, uint128 amount) public {
+        _mint(to, amount);
+    }
+}
